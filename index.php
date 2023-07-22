b165bffdacb2ee7a29acd1c90fdad7cdfa68ba5f
dji777 <101763204+dji777@users.noreply.github.com>
2023-07-22 23:51:52 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+include_once('shared/header.php');
+
+include_once('shared/produits.php');
+
+include_once('shared/footer.php');
---
+++ shared/footer.php
@@ -0,0 +1,10 @@
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; CORP 2023</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="http://localhost/shop/assets/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="http://localhost/shop/assets/js/script.js"></script>
+    </body>
+</html>
